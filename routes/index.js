f678f9b03728aea396304b0ef775d22c2bea25c0
Jesse Cannon <jessecannon@Jesses-MacBook-Air.local>
2018-02-13 13:57:22 -0500

    Added in the header, footer and all other pages

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,43 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { 
+    title: 'Jesse Cannon Portfolio',
+    message: 'Welcome to my Portfolio'
+   });
+});
+
+/* GET about page. */
+router.get('/', (req, res, next) => {
+  res.render('about', { 
+    title: 'About Me',
+    message: 'My name is Jesse Cannon and here is my portfolio with all of my best work.'
+   });
+});
+
+/* GET projects page. */
+router.get('/', (req, res, next) => {
+  res.render('projects', { 
+    title: 'Projects',
+    message: 'Here are all of my projects.'
+   });
+});
+
+/* GET services page. */
+router.get('/', (req, res, next) => {
+  res.render('services', { 
+    title: 'Services',
+    message: 'Here are my services.'
+   });
+});
+
+/* GET contact page. */
+router.get('/', (req, res, next) => {
+  res.render('contact', { 
+    title: 'Contact Me',
+    message: 'Here is my contact info.'
+   });
 });
 
 module.exports = router;
